18919864a79f17356b650b34381aedf4a1caa89e
Yaneizy Castillo <yccastillo18@gmail.com>
2021-10-26 15:47:03 -0400

    added header and navbar

---
+++ pillowtalk-frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Header from './Component/Header/Header.js';
+
+function App() {
+  return (
+
+    <div className="App">
+
+      
+    <Header />
+
+    </div>
+
+
+    
+  );
+}
+
+export default App;
---
+++ pillowtalk-frontend/src/Component/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar.js'
+
+function Header(){
+  return(
+    <div>
+
+    <h1>PilowTalk</h1>
+    <img src="https://www.vhv.rs/dpng/d/118-1189537_clouds-png-animated-cute-cartoon-cloud-png-transparent.png" alt="header_pic"/>
+     
+     <Navbar />
+
+    </div>
+  );
+}
+
+export default Header;
